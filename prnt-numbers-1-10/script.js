45b3563ec561a815b63367482962c6af0dcdd98e
David Meskhishvili <dmeskh@gmail.com>
2024-03-16 00:43:50 -0700

    Final touch up

--- prnt-numbers-1-10/script.js
+++ prnt-numbers-1-10/script.js
@@ -7,7 +7,7 @@
 //Declare empty string
 let myString = "";
 //Again use for loop 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i ++) {
     //In this case += is not doing concatenation. Just numeric addition 
     myString += i;
     //Inserting results into <div></div> tags and display in the browser
