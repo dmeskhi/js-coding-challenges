10a2b9f86fe65546d0037f30af0252a585d2def0
David Meskhishvili <dmeskh@gmail.com>
2024-03-23 23:34:33 -0700

    Update

--- array-cardio-practice/script.js
+++ array-cardio-practice/script.js
@@ -98,6 +98,7 @@ console.table(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+
 // const ordered = inventors.sort(function(a, b){
 //     if (a.year > b.year) {
 //         return 1;
