a2816a48fb523139761f74e96118915368090d0c
David Meskhishvili <dmeskh@gmail.com>
2024-03-27 20:45:47 -0700

    Changes

--- array-cardio-practice/script.js
+++ array-cardio-practice/script.js
@@ -81,19 +81,21 @@ const data = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const myFifteen = inventors.filter(
-  (inventor) => inventor.year >= 1500 && inventor.year < 1600
-);
+const myFifteen = inventors.filter(function(inventor) {
+  if (inventor.year >= 1500 && inventor.year <= 1600) {
+    return true;
+  }
+}); 
+
 console.table(myFifteen);
+
 //Display result in the browser
 //document.getElementById("myYear").innerHTML = JSON.stringify.table(myFifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
-const fullNames = inventors.map(
-  (inventor) => `${inventor.first} ${inventor.last}`
-);
-console.table(fullNames);
+
+
 //document.getElementById("myYear").innerHTML = JSON.stringify(fullNames);
 
 // Array.prototype.sort()
@@ -108,24 +110,14 @@ console.table(fullNames);
 // });
 
 //Use ternary operator
-const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
 
-console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
-const totalYears = inventors.reduce((total, inventor) => {
-  return total + (inventor.passed - inventor.year);
-}, 0);
-console.log(totalYears);
+
 
 // 5. Sort the inventors by years lived
-const oldest = inventors.sort(function (a, b) {
-  const lastGuy = a.passed - a.year;
-  const nextGuy = b.passed - b.year;
-  return lastGuy < nextGuy ? 1 : -1;
-});
-console.table(oldest);
+
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
@@ -137,20 +129,8 @@ console.table(oldest);
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
-const alpha = people.sort((lastOne, nextOne) => {
-  const [aLast, aFirst] = lastOne.split(", ");
-  const [bLast, bFirst] = lastOne.split(", ");
-  return aLast > bLast ? 1 : -1;
-});
-console.log(alpha);
+
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
-const transportation = data.reduce((obj, item) => {
-  if (!obj[item]) {
-    obj[item] = 0;
-  }
-  obj[item]++;
-  return obj;
-}, {});
-console.log(transportation);
+
